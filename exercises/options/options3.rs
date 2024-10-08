876fd83c1657d841c2134b85a9450478cd5754d3
Tom_Cat <maoy896@gmail.com>
2024-10-08 15:11:55 +0800

    complete the option exercise

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -12,12 +12,9 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => {
-            println!("Co-ordinates are {},{} ", p.x, p.y);
-        },
-        _ => {
-            panic!("no match!");
-        }
+        Some(ref p) => println!("Coordinates are {}, {}", p.x, p.y),
+        None => println!("No coordinates"),
     }
+
     y; // Fix without deleting this line.
 }
